3c7820547962a41b5da7b1d64d1aa64cbfd9d9ac
ⒹⒶⓃⒾⒺⓁ ⓀⓄⒷⓏⒾⓈⓉⓎⒾ <danyil.kobzystyi@gmail.com>
2026-06-12 20:49:34 +0300

    Обновление Android APK до v0.4.7 на странице загрузок.

--- js/downloads.js
+++ js/downloads.js
@@ -1,5 +1,5 @@
 (function () {
-  const LOCAL_APK = "/downloads/cyblight-android-v0.4.6.apk";
+  const LOCAL_APK = "/downloads/cyblight-android-v0.4.7.apk";
   const GITHUB_RELEASES =
     "https://github.com/CybLight/CybLight-Android/releases/latest";
   const GITHUB_API =
